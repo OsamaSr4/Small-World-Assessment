d4e4d3dc2a07054444b86a094711775e22d3063f
Osama Iqbal <osama.iqbal@synapse-consulting.com.pk>
2024-03-09 13:56:42 +0500

    Initial Commit

---
+++ Small World Assessment/Small World Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Small World Assessment
+//
+//  Created by MacBook Pro on 09/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
